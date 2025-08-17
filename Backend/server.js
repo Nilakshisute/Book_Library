3ec7103c1b3832614d3314abc17fb3a435518d3d
Nilakshisute <nilakshisute2020@gmail.com>
2025-08-17 16:04:36 +0530

    baseURL updated

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://book-library-w5e5.vercel.app/'],
   credentials: true 
 }));
 app.use(express.json());
--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://book-library-vef5.onrender.com',
     timeout: 20000,
     headers: {
         'Content-Type': 'application/json'
