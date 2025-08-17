cdb34c225a2d1c3eb8967395774321dac222412a
Nilakshisute <nilakshisute2020@gmail.com>
2025-08-17 16:09:14 +0530

    cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -15,11 +15,34 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://book-library-w5e5.vercel.app/'],
-  credentials: true 
-}));
+// // Middleware
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://book-library-w5e5.vercel.app/'],
+//   credentials: true 
+// }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://book-library-w5e5.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 app.use(cookieParser());
 
